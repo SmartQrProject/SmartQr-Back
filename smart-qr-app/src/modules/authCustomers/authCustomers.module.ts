6a334ec29277fbb27a3d942b712bdc3df5937ab7
geamigo <amigogabrielernesto@gmail.com>
2025-05-14 08:14:23 -0300

    Ver 2025-05-14 Ver00

--- smart-qr-app/src/modules/authCustomers/authCustomers.module.ts
+++ smart-qr-app/src/modules/authCustomers/authCustomers.module.ts
@@ -1,3 +1,5 @@
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy4Auth0 } from 'src/common/services/jwt.strategy4Auth0';
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { requiresAuth } from 'express-openid-connect';
@@ -10,13 +12,13 @@ import { RestaurantsService } from '../restaurants/restaurants.service';
 import { Restaurant } from 'src/shared/entities/restaurant.entity';
 import { RestaurantsModule } from '../restaurants/restaurants.module';
 import { CommonModule } from 'src/common/common.module';
-import { JwtStrategy4Auth0 } from 'src/common/services/jwt.strategy4Auth0';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([Customer]),
     RestaurantsModule,
     CommonModule,
+    PassportModule,
   ],
   providers: [AuthCustomersService, AuthCustomersRepository, JwtStrategy4Auth0],
   controllers: [AuthCustomersController],

65b983cc615c181508b9d6fcbc3fb8f87463d4ec
MatiasGGallardo <matiasgallardo196@gmail.com>
2025-05-23 22:50:35 +1000

    Agregar propiedad opcional 'status' al DTO de actualización de órdenes

--- smart-qr-app/src/modules/orders/dto/update-order.dto.ts
+++ smart-qr-app/src/modules/orders/dto/update-order.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateOrderDto } from './create-order.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateOrderDto extends PartialType(CreateOrderDto) {}
+export class UpdateOrderDto extends PartialType(CreateOrderDto) {
+  @IsOptional()
+  @IsString()
+  status?: 'pending' | 'in-process' | 'ready' | 'completed';
+}

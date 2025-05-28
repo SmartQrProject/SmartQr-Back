c0ae269bb510a2492af3b750260bc14ce6842a4d
SmartQrProject <smartqr2@gmail.com>
2025-05-28 13:16:50 -0500

    Actualizar patch-restaurants.dto.ts

--- smart-qr-app/src/modules/restaurants/dto/patch-restaurants.dto.ts
+++ smart-qr-app/src/modules/restaurants/dto/patch-restaurants.dto.ts
@@ -3,5 +3,5 @@ import { CompleteRestaurantsDto } from './complete-restaurants.dto';
 import { ApiProperty, PartialType, PickType } from '@nestjs/swagger';
 
 export class PatchRestaurantsDto extends PartialType(
-  PickType(CompleteRestaurantsDto, ['name', 'banner', 'address', 'phone', 'description', 'tags', 'trading_hours', 'ordering_times', 'longitude', 'longitude']),
+  PickType(CompleteRestaurantsDto, ['name', 'banner', 'address', 'phone', 'description', 'tags', 'trading_hours', 'ordering_times', 'longitude', 'latitude']),
 ) {}

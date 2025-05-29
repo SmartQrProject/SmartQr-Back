6fc49e400d69cf9aa7b77669c0a7c2ce5862e7a0
MatiasGGallardo <matiasgallardo196@gmail.com>
2025-05-29 20:32:25 +1000

    Añadir SubscriptionController al módulo de suscripción

--- smart-qr-app/src/modules/subscription/subscription.module.ts
+++ smart-qr-app/src/modules/subscription/subscription.module.ts
@@ -4,10 +4,12 @@ import { SubscriptionService } from './subscription.service';
 import { Subscription } from 'src/shared/entities/subscription.entity';
 import { Restaurant } from 'src/shared/entities/restaurant.entity';
 import { StripeModule } from '../stripe/stripe.module';
+import { SubscriptionController } from './subscription.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Subscription, Restaurant]), StripeModule],
   providers: [SubscriptionService],
+  controllers: [SubscriptionController],
   exports: [SubscriptionService],
 })
 export class SubscriptionModule {}
